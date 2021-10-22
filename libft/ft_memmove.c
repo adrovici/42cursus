adb47d6d7444ba1a6eba7a3c7570087db25e0917
PILTRAFILLA317 <unaimartinm@gmail.com>
2021-10-22 12:01:21 +0200

    tested size

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,13 +6,13 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 11:20:55 by umartin-          #+#    #+#             */
-/*   Updated: 2021/10/22 11:49:21 by umartin-         ###   ########.fr       */
+/*   Updated: 2021/10/22 11:59:50 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	int	i;
 
@@ -20,7 +20,7 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 		return (NULL);
 	if (dst > (void *)src)
 	{
-		i = (int)n - 1;
+		i = (int)len - 1;
 		while (i >= 0)
 		{
 			*(char *)(dst + i) = *(char *)(src + i);
@@ -30,7 +30,7 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	else
 	{
 		i = 0;
-		while (i < (int)n)
+		while (i < (int)len)
 		{
 			*(char *)(dst + i) = *(char *)(src + i);
 			i++;
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 11:16:24 by umartin-          #+#    #+#             */
-/*   Updated: 2021/10/22 11:53:14 by umartin-         ###   ########.fr       */
+/*   Updated: 2021/10/22 11:59:25 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 void	ft_bzero(void *b, size_t len);
 
 int		ft_isalnum(int c);
