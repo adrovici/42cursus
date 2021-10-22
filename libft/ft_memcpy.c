2ddb67f18af3f07a3dc36ec6333b10ceba110e79
PILTRAFILLA317 <unaimartinm@gmail.com>
2021-10-22 11:02:42 +0200

    memcpy modify

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,10 +6,10 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 10:49:31 by umartin-          #+#    #+#             */
-/*   Updated: 2021/10/22 10:59:26 by umartin-         ###   ########.fr       */
+/*   Updated: 2021/10/22 11:02:07 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
