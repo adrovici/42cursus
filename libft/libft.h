c83bf0146da00cbdea46914b305a532ca39f2ca9
PILTRAFILLA317 <unaimartinm@gmail.com>
2021-10-22 13:49:55 +0200

    tolower

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 11:16:24 by umartin-          #+#    #+#             */
-/*   Updated: 2021/10/22 13:19:02 by umartin-         ###   ########.fr       */
+/*   Updated: 2021/10/22 13:41:32 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int		ft_isprint(int c);
 int		ft_isdigit(int c);
 int		ft_isascii(int c);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
