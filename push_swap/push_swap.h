41e168d8daf2c200ad5a04b56ce7e90a52b44fc9
PILTRAFILLA317 <unaimartinm@gmail.com>
2022-03-08 15:13:10 +0100

    sa, sb and ss moves

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 16:43:23 by umartin-          #+#    #+#             */
-/*   Updated: 2022/03/07 19:41:14 by umartin-         ###   ########.fr       */
+/*   Updated: 2022/03/08 15:08:43 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,8 @@ int		checker(int ac, char **av);
 void	errormsg(void);
 int		ft_atoi_psw(const char *str);
 void	addnumbers(t_list **sta, char **av, int i);
+void	psw_sa(t_list **sta);
+void	psw_sb(t_list **stb);
+void	psw_ss(t_list **sta, t_list **stb);
 
 #endif
--- push_swap/push_swap_smoves.c
+++ push_swap/push_swap_smoves.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap_moves.c                                  :+:      :+:    :+:   */
+/*   push_swap_smoves.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 18:46:03 by umartin-          #+#    #+#             */
-/*   Updated: 2022/03/07 18:46:48 by umartin-         ###   ########.fr       */
+/*   Updated: 2022/03/08 15:11:55 by umartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,37 @@
 
 void	psw_sa(t_list **sta)
 {
-	
+	t_list	*temp1;
+	t_list	*temp2;
+
+	if (*sta == NULL)
+		return ;
+	temp1 = *sta;
+	temp2 = temp1->next;
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+	*sta = temp2;
+	ft_printf("sa\n");
+}
+
+void	psw_sb(t_list **stb)
+{
+	t_list	*temp1;
+	t_list	*temp2;
+
+	if (*stb == NULL)
+		return ;
+	temp1 = *stb;
+	temp2 = temp1->next;
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+	*stb = temp2;
+	ft_printf("sb\n");
+}
+
+void	psw_ss(t_list **sta, t_list **stb)
+{
+	psw_sa(&sta);
+	psw_sb(&stb);
+	ft_printf("ss\n");
 }
