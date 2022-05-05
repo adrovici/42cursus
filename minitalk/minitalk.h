772632dfd147f9ef24eac326ef1ef05ad8d8d22c
PILTRAFILLA317 <unaimartinm@gmail.com>
2022-05-05 10:58:31 +0200

    minitalk pid

---
+++ minitalk/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: umartin- <umartin-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 14:25:18 by umartin-          #+#    #+#             */
+/*   Updated: 2022/05/02 16:06:01 by umartin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "libft/libft.h"
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <signal.h>
+
+#endif
